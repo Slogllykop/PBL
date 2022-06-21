2a58fb556f58341dc1d0b767e7caab8dd19316d8
Slogllykop <slogllykop07@gmail.com>
2022-06-21 17:18:18 +0530

    User Account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+// Responsive navbar button
 let navbar = document.querySelector('.header .flex .navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 window.onscroll = () =>{
     navbar.classList.remove('active');
+}
+
+// User account toggle
+let acc = document.querySelector('.user-account');
+document.querySelector('#user-btn').onclick = () =>{
+    acc.classList.add('active');
+}
+document.querySelector('#close-account').onclick = () =>{
+    acc.classList.remove('active');
 }
