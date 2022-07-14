e0078320ea92d2b82b6b6c09a370f6ed9673bde2
Slogllykop <slogllykop07@gmail.com>
2022-07-15 00:00:07 +0530

    dark mode

---
+++ src/darkModeToggle.js
@@ -0,0 +1,21 @@
+let darkMode = localStorage.getItem('darkMode'); 
+const darkModeToggle = document.querySelector('#theme');
+const enableDarkMode = () => {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('darkMode', 'enabled');
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('darkMode', null);
+}
+if (darkMode === 'enabled') {
+enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {  
+        disableDarkMode(); 
+    }
+});
